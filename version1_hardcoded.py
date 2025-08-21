3b57eee551affc8953c12f77a02eb69e0bc5e98c
Abhinay <amente@gitam.in>
2025-08-21 11:11:02 +0530

    Create version1_hardcoded.py

---
+++ version1_hardcoded.py
@@ -0,0 +1,13 @@
+a = -0.3
+b = 4.5
+c = 15.0
+
+
+t = 8
+
+temperature = a * t**2 + b * t + c
+
+print("Weather Prediction Model")
+print("-------------------------")
+print(f"Time (t) = {t}")
+print(f"Predicted temperature: {temperature:.2f}°C")
